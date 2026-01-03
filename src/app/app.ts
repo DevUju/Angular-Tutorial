af6da3056144b5543804ff5c26625e7d6fa76d8e
Agnes Anosike <agnesanosike@gmail.com>
2026-01-03 18:07:55 +0100

    feat: counter app with increase, decrease, and reset feature.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('counter-app');
+
+  count:number = 0
+
+  increase (){
+    this.count = this.count + 1
+  }
+
+  decrease (){
+    if (this.count === 0) {
+      this.count = 0
+    } else {
+    this.count = this.count - 1
+    }
+  }
+
+  reset (){
+    this.count = 0
+  }
+
+  counterFunction (val:string){
+    if (val === 'plus'){
+      this.count = this.count + 1
+    } else if (val === 'minus'){
+      if (this.count === 0) {
+        this.count = 0
+      } else {
+      this.count = this.count - 1
+      }
+  } else if (val === 'reset'){
+    this.count = 0
+  }
+}
 }

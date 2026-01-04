0316c5e10dbe10329eb3ef48f80c2b50073a8d38
Agnes Anosike <agnesanosike@gmail.com>
2026-01-04 15:55:37 +0100

    feat: setting input and if else toggle practice

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,35 +9,70 @@ import { Component, signal } from '@angular/core';
 export class App {
   protected readonly title = signal('counter-app');
 
-  count:number = 0
+//   count:number = 0
 
-  increase (){
-    this.count = this.count + 1
+//   increase (){
+//     this.count = this.count + 1
+//   }
+
+//   decrease (){
+//     if (this.count === 0) {
+//       this.count = 0
+//     } else {
+//     this.count = this.count - 1
+//     }
+//   }
+
+//   reset (){
+//     this.count = 0
+//   }
+
+//   counterFunction (val:string){
+//     if (val === 'plus'){
+//       this.count = this.count + 1
+//     } else if (val === 'minus'){
+//       if (this.count === 0) {
+//         this.count = 0
+//       } else {
+//       this.count = this.count - 1
+//       }
+//   } else if (val === 'reset'){
+//     this.count = 0
+//   }
+// }
+
+//   username:string = ''
+
+//   getUserName (event:Event) {
+//     this.username = (event.target as HTMLInputElement).value
+//     console.log(this.username)
+//   }
+
+//   setUserName () {
+//     this.username = "Obianuju"
+// }
+
+//   getUserNameField (val:string) {
+//     this.username = val
+//     console.log(this.username)
+// }
+
+  display = true
+  displayOneDiv = true
+
+  hide(){
+    this.display = false
   }
 
-  decrease (){
-    if (this.count === 0) {
-      this.count = 0
-    } else {
-    this.count = this.count - 1
-    }
+  show(){
+    this.display = true
   }
 
-  reset (){
-    this.count = 0
+  toggleDisplay () {
+    this.display = !this.display
   }
 
-  counterFunction (val:string){
-    if (val === 'plus'){
-      this.count = this.count + 1
-    } else if (val === 'minus'){
-      if (this.count === 0) {
-        this.count = 0
-      } else {
-      this.count = this.count - 1
-      }
-  } else if (val === 'reset'){
-    this.count = 0
+  displayDiv () {
+    this.displayOneDiv = !this.displayOneDiv
   }
-}
 }
